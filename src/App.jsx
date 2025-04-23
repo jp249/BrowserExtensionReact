c37a93d1ac4365d3bcd00bee62c944672ce472db
jp249 <vangipierre18@gmail.com>
2025-04-23 19:32:29 -0400

    Fixed Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Card from "./Card"
 
 
 function App() {
-  return(<Card>
-    
+  return(<Card body3 = "spongebob">
+
   </Card>)
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,13 @@
-function Card () {
+function Card (props) {
   return(
     <div className="Card">
-      <img className="cardimage" src={logoPicture} alt="profile picture"></img>
+      <img className="cardimage"  alt="extension icon"></img>
       <h2 className="header">Header</h2>
-      <p className='body'>Description</p>
+      <p className='body'>{props.body3}</p>
     </div>
   )
 }
 
 export default Card
+
+//{description2.body3}
