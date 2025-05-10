302742fbde79dd8576d8c5dd717a2052c9d35044
jp249 <vangipierre18@gmail.com>
2025-05-10 14:54:44 -0400

    CSS edited more

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,21 @@
 function Card (props) {
-  return(
+  return(<>
     <div className="Card">
       <img className="cardimage"  alt="extension icon" src={props.image}></img>
       <h2 className="header">{props.header}</h2>
       <p className='body'>{props.paragraph}</p>
+      <label class="switch">
+      <input type="checkbox"/>
+      <span class="slider round"></span>
+      </label>
+      <div>
+      <button class="removeButton">REMOVE</button>
+      </div>
     </div>
+    </>
+    
   )
 }
-
 export default Card
 
 //{description2.body3}
